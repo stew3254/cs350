10504a10876e53b820ae4712c70b3c88b46c4544
stew3254 <rtstewy@gmail.com>
2021-10-22 22:03:33 -0400

    Add Docker support, environment files, readme and try to get server running.

--- README.md
+++ README.md
@@ -5,3 +5,19 @@ CS350 term project
 ## Team website
 
 A team website with all relevant files is located at <http://condor-team.cslabs.clarkson.edu>
+
+## Quickstart
+
+To get started, copy the `env-template` file over to `.env`. Next, fill in secure passwords in the `.env` file
+
+## Running
+
+To run the application, you can either run `docker-compose up --build` or depending on yoru operating system, the `start.sh` and `start.bat` files.
+
+The portal will be located at http://localhost:8000
+
+If you would like to stop the application, just run
+
+```
+docker-compose stop
+```
--- src/cs350/settings.py
+++ src/cs350/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -79,8 +80,11 @@ WSGI_APPLICATION = 'cs350.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': os.getenv('DB_SERVER'),
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
     }
 }
 
