0b6fe18fcced8b71d239fb4f3099c9e84eb33bbe
Stew <rtstewy@gmail.com>
2021-11-24 10:56:40 -0500

    Add basic authentication. Needs to be fixed to actually follow requirements specs and attach to students (#15)

--- src/cs350/settings.py
+++ src/cs350/settings.py
@@ -16,7 +16,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ SECRET_KEY = 'django-insecure-(hx*wka_%*s12xb(!+#0d_mq0a9o=-l_o28oh+2%b*5z48nz$m
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -74,7 +72,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'cs350.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -87,7 +84,6 @@ DATABASES = {
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -107,7 +103,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -121,7 +116,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
@@ -131,3 +125,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
--- src/majorizer/admin.py
+++ src/majorizer/admin.py
@@ -3,7 +3,12 @@ from django.contrib import admin
 # Register your models here.
 from .models import *
 admin.site.register(Department)
+admin.site.register(Course)
+admin.site.register(CourseOffering)
+admin.site.register(DegreeProgram)
 admin.site.register(User)
 admin.site.register(Student)
+admin.site.register(Schedule)
 admin.site.register(Advisor)
 admin.site.register(AdvisorCode)
+admin.site.register(Comment)
