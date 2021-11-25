659d6a4e7e642a641888315efc86eb01e4205c0a
Jacob Desilets <jacobdesilets@gmail.com>
2021-11-24 23:28:31 -0500

    Layed the foundations for the login button on the home page, and significantly improved the graphic design

--- src/cs350/settings.py
+++ src/cs350/settings.py
@@ -120,9 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -131,6 +129,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
-
-import mimetypes
-mimetypes.add_type("text/css", ".css", True)
--- src/majorizer/views.py
+++ src/majorizer/views.py
@@ -1,7 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .forms import LoginForm
+
 # Create your views here.
 def home_view(request):
+    form = LoginForm()
+
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['username']
+            form = LoginForm(request.POST)
+            return render(request, "home.html", {'form' : form, 'name' : name})
+
     #return HttpResponse("<h1>Hello World</h1>")
-    return render(request, "home.html", {})
+    return render(request, "home.html", {'form' : form, 'name' : "NONE"})
+
